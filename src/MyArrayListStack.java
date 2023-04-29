9c78f15d06059cb3ee771df62c0a15c3c2871b60
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-29 22:05:32 +0600

    Import the necessary classes: java.util.EmptyStackException for the stack exceptions

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,6 +1,10 @@
+import java.util.EmptyStackException;
 public class MyArrayListStack<T> {
     MyArrayList<T> stack;
     public MyArrayListStack() {
         stack = new MyArrayList<>();
+    }
+    public void push(T element){
+        stack.add(element);
     }
 }
