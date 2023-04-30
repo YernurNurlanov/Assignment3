cea9cc0b24d9b1faf77da65850492ada8f8336a6
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-30 21:25:12 +0600

    Implement the pop method that removes and returns the top element of the stack using the remove method for MyArrayList.

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -5,9 +5,16 @@ public class MyArrayListStack<T> {
         stack = new MyArrayList<>();
     }
     public void push(T element){
-        stack.add(element, 0);
+        stack.add(element);
     }
     public boolean isEmpty() {
         return stack.size() == 0;
+    }
+    public T pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        stack.remove(stack.size() - 1);
+        return stack.get(stack.size() - 1);
     }
 }
