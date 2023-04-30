901452e2a0242b0d2b3a694ac6bead5ffc251b8b
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-30 22:49:05 +0600

    comments

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,27 +1,34 @@
 import java.util.EmptyStackException;
 public class MyArrayListStack<T> {
+    // stack - instance of type MyArrayList
     MyArrayList<T> stack;
+    // MyArrayListStack() - constructor for the class
     public MyArrayListStack() {
         stack = new MyArrayList<>();
     }
+    // adds an element to the top of the stack
     public void push(T element){
         stack.add(element,0);
     }
+    // returns true if the stack is empty or false if it is not
     public boolean isEmpty() {
         return stack.size() == 0;
     }
+    //removes and returns the top element of the stack
     public T pop(){
         if(isEmpty()){
             throw new EmptyStackException();
         }
         return stack.remove(0);
     }
+    //returns the top element of the stack
     public T peek(){
         if(isEmpty()){
             throw new EmptyStackException();
         }
         return stack.get(0);
     }
+    //returns the number of elements in the stack
     public int size(){
         return stack.size();
     }
--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,24 +1,31 @@
 import java.util.NoSuchElementException;
 public class MyLinkedListQueue<T> {
+    // queue - instance of type MyLinkedList
     MyLinkedList<T> queue;
+    // MyLinkedListQueue() - constructor for the class
     public MyLinkedListQueue(){
         queue = new MyLinkedList<>();
     }
+    // adds an element to the back of the queue
     public void enqueue(T element){
         queue.add(element);
     }
+    // removes and returns the front element of the queue
     public T dequeue(){
         return queue.remove(0);
     }
+    //returns the front element of the queue
     public T peek(){
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return queue.get(0);
     }
+    // returns true if the queue is empty or false if it is not
     public boolean isEmpty(){
         return queue.size() == 0;
     }
+    //returns the number of elements in the queue
     public int size(){
         return queue.size();
     }
